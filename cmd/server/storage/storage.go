9edf6223840ab97ad8a2af93e790a1663c36179c
Yakovishin Evgeny <00705@inbox.ru>
2024-10-03 15:45:05 +0300

    fix

--- cmd/server/storage/storage.go
+++ cmd/server/storage/storage.go
@@ -1,11 +1,8 @@
 package storage
 
-import "sync"
-
 type MemStorage struct {
 	gauges   map[string]float64
 	counters map[string]int64
-	mu       sync.Mutex
 }
 
 func NewMemStorage() *MemStorage {
@@ -16,39 +13,27 @@ func NewMemStorage() *MemStorage {
 }
 
 func (m *MemStorage) SetGauge(metricName string, value float64) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
 	m.gauges[metricName] = value
 }
 
 func (m *MemStorage) IncrementCounter(metricName string, value int64) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
 	m.counters[metricName] += value
 }
 
 func (m *MemStorage) GetGauge(metricName string) (float64, bool) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
 	value, exists := m.gauges[metricName]
 	return value, exists
 }
 
 func (m *MemStorage) GetCounter(metricName string) (int64, bool) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
 	value, exists := m.counters[metricName]
 	return value, exists
 }
 
 func (m *MemStorage) GetAllGauges() map[string]float64 {
-	m.mu.Lock()
-	defer m.mu.Unlock()
 	return m.gauges
 }
 
 func (m *MemStorage) GetAllCounters() map[string]int64 {
-	m.mu.Lock()
-	defer m.mu.Unlock()
 	return m.counters
 }
