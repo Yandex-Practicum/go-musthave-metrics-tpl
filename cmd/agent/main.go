532b6927ff4a72db76540c8e4c47477fa3da323a
Yakovishin Evgeny <00705@inbox.ru>
2024-10-09 23:09:09 +0300

    add gracefulShutdown for client

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"evgen3000/go-musthave-metrics-tpl.git/cmd/agent/client"
 	"flag"
+	"log"
 	"os"
+	"os/signal"
 	"strconv"
 	"time"
 )
 
+func gracefulShutdown() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		log.Println("Shutting down gracefully")
+		os.Exit(0)
+	}()
+}
+
 func main() {
+	gracefulShutdown()
+
 	hostEnv, isHostEnv := os.LookupEnv("ADDRESS")
 	reportIntervalEnv, isReportIntervalEn := os.LookupEnv("REPORT_INTERVAL")
 	pollIntervalEnv, isPollIntervalEnv := os.LookupEnv("POLL_INTERVAL")
