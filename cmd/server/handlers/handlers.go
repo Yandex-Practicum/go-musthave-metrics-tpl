560410b8254185f6cdca4c736d8371af5c713c3d
Yakovishin Evgeny <00705@inbox.ru>
2024-10-11 23:53:34 +0300

    fix: rm magic strings fom handlers.go

--- cmd/server/handlers/handlers.go
+++ cmd/server/handlers/handlers.go
@@ -5,8 +5,14 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
+)
+
+const (
+	MetricTypeCounter = "counter"
+	MetricTypeGauge   = "gauge"
 )
 
 type Handler struct {
@@ -18,18 +24,20 @@ func NewHandler(storage *storage.MemStorage) *Handler {
 }
 
 func (h *Handler) HomeHandler(rw http.ResponseWriter, r *http.Request) {
-	body := "<h4>Gauges</h4>"
+
+	var body strings.Builder
+	body.WriteString("<h4>Gauges</h4>")
 	for gaugeName, value := range h.storage.GetAllGauges() {
-		body += gaugeName + ": " + strconv.FormatFloat(value, 'f', -1, 64) + "</br>"
+		body.WriteString(gaugeName + ": " + strconv.FormatFloat(value, 'f', -1, 64) + "</br>")
 	}
-	body += "<h4>Counters</h4>"
+	body.WriteString("<h4>Counters</h4>")
 
 	for counterName, value := range h.storage.GetAllCounters() {
-		body += counterName + ": " + strconv.FormatInt(value, 10) + "</br>"
+		body.WriteString(counterName + ": " + strconv.FormatInt(value, 10) + "</br>")
 	}
 	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	_, err := rw.Write([]byte(body))
+	_, err := rw.Write([]byte(body.String()))
 	if err != nil {
 		log.Printf("Write failed: %v", err)
 	}
@@ -41,13 +49,13 @@ func (h *Handler) UpdateMetricHandler(rw http.ResponseWriter, r *http.Request) {
 	metricValue := chi.URLParam(r, "metricValue")
 
 	switch metricType {
-	case "counter":
+	case MetricTypeCounter:
 		value, err := strconv.ParseInt(metricValue, 10, 64)
 		if err != nil {
 			http.Error(rw, "Bad request", http.StatusBadRequest)
 		}
 		h.storage.IncrementCounter(metricName, value)
-	case "gauge":
+	case MetricTypeGauge:
 		value, err := strconv.ParseFloat(metricValue, 64)
 		if err != nil {
 			http.Error(rw, "Bad request", http.StatusBadRequest)
@@ -64,12 +72,11 @@ func (h *Handler) GetMetricHandler(rw http.ResponseWriter, r *http.Request) {
 	metricType := chi.URLParam(r, "metricType")
 	metricName := chi.URLParam(r, "metricName")
 
-	if metricType != "gauge" && metricType != "counter" {
+	if metricType != MetricTypeGauge && metricType != MetricTypeCounter {
 		http.Error(rw, "Invalid metric type", http.StatusBadRequest)
 	}
 
-	switch metricType {
-	case "gauge":
+	if metricType == MetricTypeGauge {
 		value, exists := h.storage.GetGauge(metricName)
 		if !exists {
 			http.Error(rw, "Metric not found", http.StatusNotFound)
@@ -79,7 +86,7 @@ func (h *Handler) GetMetricHandler(rw http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Printf("Write failed: %v", err)
 		}
-	default:
+	} else {
 		value, exists := h.storage.GetCounter(metricName)
 		if !exists {
 			http.Error(rw, "Metric not found", http.StatusNotFound)
