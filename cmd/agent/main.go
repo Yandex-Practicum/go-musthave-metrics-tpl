5674bba61b61465ed7680128890dfedf0db97f8a
Yakovishin Evgeny <00705@inbox.ru>
2024-10-13 14:40:46 +0300

    feat: add config.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"evgen3000/go-musthave-metrics-tpl.git/cmd/agent/provider"
-	"flag"
+	"evgen3000/go-musthave-metrics-tpl.git/internal/config"
 	"log"
 	"os"
 	"os/signal"
-	"strconv"
 	"time"
 )
 
@@ -22,28 +21,7 @@ func gracefulShutdown() {
 
 func main() {
 	gracefulShutdown()
-
-	hostEnv, isHostEnv := os.LookupEnv("ADDRESS")
-	reportIntervalEnv, isReportIntervalEn := os.LookupEnv("REPORT_INTERVAL")
-	pollIntervalEnv, isPollIntervalEnv := os.LookupEnv("POLL_INTERVAL")
-
-	hostFlag := flag.String("a", "localhost:8080", "Host IP address and port.")
-	reportIntervalFlag := flag.Int("r", 10, "Report interval in seconds.")
-	pollIntervalFlag := flag.Int("p", 2, "Pool interval in seconds.")
-	flag.Parse()
-
-	if isReportIntervalEn && isPollIntervalEnv && isHostEnv {
-		poolInterval, err := strconv.ParseInt(pollIntervalEnv, 10, 64)
-		if err != nil {
-			log.Fatal("Variable is not valid:", err)
-		}
-		reportInterval, err := strconv.ParseInt(reportIntervalEnv, 10, 64)
-		if err != nil {
-			log.Fatal("Variable is not valid:", err)
-		}
-		(provider.NewAgent(hostEnv, time.Duration(poolInterval)*time.Second, time.Duration((reportInterval))*time.Second)).Start()
-	} else {
-		(provider.NewAgent(*hostFlag, time.Duration(*pollIntervalFlag)*time.Second, time.Duration((*reportIntervalFlag))*time.Second)).Start()
-	}
+	c := config.GetAgentConfig()
+	(provider.NewAgent(c.Host, time.Duration(c.PoolInterval)*time.Second, time.Duration(c.ReportInterval)*time.Second)).Start()
 
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"flag"
+	"log"
 	"os"
+	"strconv"
 )
 
 type HostConfig struct {
@@ -10,19 +12,56 @@ type HostConfig struct {
 	IsEnv bool
 }
 
-func GetHost() HostConfig {
+func GetHost() *HostConfig {
 	hostFlag := flag.String("a", "localhost:8080", "Host IP address and port.")
 	flag.Parse()
 
 	env, isEnv := os.LookupEnv("ADDRESS")
 	if isEnv {
-		return HostConfig{
+		return &HostConfig{
 			Value: env,
 			IsEnv: isEnv,
 		}
 	}
-	return HostConfig{
+	return &HostConfig{
 		Value: *hostFlag,
 		IsEnv: false,
+	}
+}
+
+type AgentConfig struct {
+	PoolInterval   int64
+	ReportInterval int64
+	Host           string
+}
+
+func GetAgentConfig() *AgentConfig {
+	reportIntervalEnv, isReportIntervalEnv := os.LookupEnv("REPORT_INTERVAL")
+	pollIntervalEnv, isPollIntervalEnv := os.LookupEnv("POLL_INTERVAL")
+
+	reportIntervalFlag := flag.Int("r", 10, "Report interval in seconds.")
+	pollIntervalFlag := flag.Int("p", 2, "Pool interval in seconds.")
+	host := GetHost()
+
+	if host.IsEnv && isPollIntervalEnv && isReportIntervalEnv {
+		pollInterval, err := strconv.ParseInt(pollIntervalEnv, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		reportInterval, err := strconv.ParseInt(reportIntervalEnv, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return &AgentConfig{
+			PoolInterval:   pollInterval,
+			ReportInterval: reportInterval,
+			Host:           host.Value,
+		}
+	}
+	return &AgentConfig{
+		PoolInterval:   int64(*pollIntervalFlag),
+		ReportInterval: int64(*reportIntervalFlag),
+		Host:           host.Value,
 	}
 }
