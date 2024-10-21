aa457a1f5866d16a5b766b2b8af95367848677b6
Yakovishin Evgeny <00705@inbox.ru>
2024-10-21 14:54:40 +0300

    feat: add compressorMiddleware for server

--- cmd/server/router/router.go
+++ cmd/server/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"evgen3000/go-musthave-metrics-tpl.git/cmd/server/handlers"
 	"evgen3000/go-musthave-metrics-tpl.git/cmd/server/storage"
+	"evgen3000/go-musthave-metrics-tpl.git/internal/compressor"
 	"evgen3000/go-musthave-metrics-tpl.git/internal/logger"
 	"github.com/go-chi/chi/v5"
 )
@@ -11,6 +12,8 @@ func SetupRouter(storage *storage.MemStorage) *chi.Mux {
 	h := handlers.NewHandler(storage)
 	chiRouter := chi.NewRouter()
 	chiRouter.Use(logger.LoggingMiddleware)
+	chiRouter.Use(compressor.GzipMiddleware)
+
 	chiRouter.Route("/update", func(r chi.Router) {
 		r.Post("/", h.UpdateMetricHandlerJSON)
 		r.Post("/{metricType}/{metricName}/{metricValue}", h.UpdateMetricHandlerText)
---
+++ internal/compressor/compressor.go
@@ -0,0 +1,48 @@
+package compressor
+
+import (
+	"compress/gzip"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func GzipMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request URL: %s, Content-Encoding: %s, Accept-Encoding: %s", r.URL, r.Header.Get("Content-Encoding"), r.Header.Get("Accept-Encoding"))
+
+		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			gzipReader, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, "Unable to read gzip data", http.StatusBadRequest)
+				return
+			}
+			defer gzipReader.Close()
+
+			r.Body = io.NopCloser(gzipReader)
+		}
+
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			gz := gzip.NewWriter(w)
+			defer gz.Close()
+
+			w.Header().Set("Content-Encoding", "gzip")
+			w.Header().Set("Vary", "Accept-Encoding")
+
+			gzw := &gzipResponseWriter{ResponseWriter: w, Writer: gz}
+			next.ServeHTTP(gzw, r)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	Writer io.Writer
+}
+
+func (g *gzipResponseWriter) Write(b []byte) (int, error) {
+	return g.Writer.Write(b)
+}
