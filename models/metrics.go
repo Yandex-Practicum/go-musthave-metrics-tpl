2ca2e3e5a396fd97ded66ca767f8d7491debee6a
Alexander Demidenko <hurd@yandex-team.ru>
2025-05-28 08:34:51 +0700

    update project structure

---
+++ models/metrics.go
@@ -0,0 +1,19 @@
+package models
+
+const (
+	Counter = "counter"
+	Gauge   = "gauge"
+)
+
+// NOTE: Не усложняем пример, вводя иерархическую вложенность структур.
+// Органичиваясь плоской моделью.
+// Delta и Value объявлены через указатели,
+// что бы отличать значение "0", от не заданного значения
+// и соответственно не кодировать в структуру.
+type Metrics struct {
+	ID    string   `json:"id"`
+	MType string   `json:"type"`
+	Delta *int64   `json:"delta,omitempty"`
+	Value *float64 `json:"value,omitempty"`
+	Hash  string   `json:"hash,omitempty"`
+}
